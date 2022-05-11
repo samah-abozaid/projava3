8be7b38e4a336997f0d732dd222dc99371bce720
samah-abozaid <samah.abozaid89@gmail.com>
2022-05-11 20:41:25 +0200

    deuxiem code

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -1,22 +1,28 @@
 package fr.m2i;
 import java.util.Scanner;
-
+import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int nbOne;
         int nbTwo;
         int nbThree;
-        System.out.print("Entrer le premier nombre : ");
-        nbOne = scanner.nextInt();
-        System.out.print("Entrer le deuxieme nombre : ");
-        nbTwo = scanner.nextInt();
-        scanner.close();
-        nbThree = nbOne * nbTwo;
-        String resultToFormat = "Résultat : %d * %d = %d";
-        String result = String.format(resultToFormat, nbOne, nbTwo, nbThree);
+        try {
+            System.out.print("Entrer le premier nombre : ");
+            nbOne = scanner.nextInt();
+            System.out.print("Entrer le deuxieme nombre : ");
+            nbTwo = scanner.nextInt();
+            scanner.close();
+            nbThree = nbOne * nbTwo;
+            String resultToFormat = "Résultat : %d * %d = %d";
+            String result = String.format(resultToFormat, nbOne, nbTwo, nbThree);
 
-        System.out.println(result);
+            System.out.println(result);
+        } catch (InputMismatchException e) {
+            System.out.println("Attention ! Tu es sencé mettre un nombre entier");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
