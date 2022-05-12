a811f7439104c90d006e2e075970774abc28555c
samah-abozaid <samah.abozaid89@gmail.com>
2022-05-12 11:51:38 +0200

    addition

--- src/main/java/fr/m2i/Main.java
+++ src/main/java/fr/m2i/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
 
+Calculator calc=new Calculator();
+     int result =calc.add(12,18);
+     System.out.println(result);
+
 
 
     }
